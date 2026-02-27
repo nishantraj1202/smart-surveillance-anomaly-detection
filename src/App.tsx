eef4bbfa83f430137054dd993ebb8e3e63c60069
Nishant <nishantraj2077@gmail.com>
2026-02-27 22:47:18 +0530

    refactor: restructure project, components, and variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/core/components/ui/toaster";
+import { Toaster as Sonner } from "@/core/components/ui/sonner";
+import { TooltipProvider } from "@/core/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import Error404 from "./pages/Error404";
 
 const queryClient = new QueryClient();
 
@@ -15,9 +15,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Home />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
---
+++ src/core/components/ui/use-toast.ts
@@ -0,0 +1,3 @@
+import { useToast, toast } from "@/core/hooks/use-toast";
+
+export { useToast, toast };
